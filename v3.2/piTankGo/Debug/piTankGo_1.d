d4459d2ec57fab7880be37630613d38fe238d29f
gonzalo moreno arevalo <gonzalomoreno05@gmail.com>
2019-03-11 17:09:29 +0100

    version 3.2

---
+++ v3.2/piTankGo/Debug/piTankGo_1.d
@@ -0,0 +1,24 @@
+piTankGo_1.o: ../piTankGo_1.c ../piTankGo_1.h \
+ C:\SysGCC\Raspberry\include\wiringPi/wiringPi.h ../kbhit.h ../player.h \
+ C:\SysGCC\Raspberry\include\wiringPi/softTone.h ../tmr.h \
+ ../piTankGoLib.h ../fsm.h ../torreta.h ../teclado.h
+
+../piTankGo_1.h:
+
+C:\SysGCC\Raspberry\include\wiringPi/wiringPi.h:
+
+../kbhit.h:
+
+../player.h:
+
+C:\SysGCC\Raspberry\include\wiringPi/softTone.h:
+
+../tmr.h:
+
+../piTankGoLib.h:
+
+../fsm.h:
+
+../torreta.h:
+
+../teclado.h:
