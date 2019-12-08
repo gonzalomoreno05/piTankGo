89826a5d2f8303c4aa16f8833a49fc97c397e549
gonzalo moreno arevalo <gonzalomoreno05@gmail.com>
2019-12-08 18:33:57 +0100

    Version final con mejoras 2.0

---
+++ 2.0/torreta/Debug/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../fsm.c \
+../kbhit.c \
+../servo_basico.c 
+
+OBJS += \
+./fsm.o \
+./kbhit.o \
+./servo_basico.o 
+
+C_DEPS += \
+./fsm.d \
+./kbhit.d \
+./servo_basico.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	arm-linux-gnueabihf-gcc -I"C:\SysGCC\Raspberry\include" -I"C:\SysGCC\Raspberry\include\wiringPi" -O0 -g3 -Wall -Werror -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
